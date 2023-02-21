4152a91fc2d3d7156b67d3532ec30954773ee1fb
Thais <cintraathais@gmail.com>
2023-02-20 23:32:01 -0300

    componentes

--- src/componentes/Menu/index.jsx
+++ src/componentes/Menu/index.jsx
@@ -6,31 +6,32 @@ import novas from '../../assets/assets/icones/novas.png'
 import surpreenda from '../../assets/assets/icones/surpreenda.png'
 import styles from './Menu.module.scss';
 
+
 export default function Menu() {
     return (
     <nav className={styles.menu}>
         <ul className={styles.menu__lista}>
             <li className={styles.menu__item}>
-                <img scr={home} alt="" />
+                <img src={home} alt="" />
                 <a href="/">Início</a>
             </li>
             <li className={styles.menu__item}>
-                <img scr={mais1} alt="" />
+                <img src={mais1} alt="" />
                 <a href="/">Mais curtidas</a>
             </li>
             <li className={styles.menu__item}>
-                <img scr={mais} alt="" />
+                <img src={mais} alt="" />
                 <a href="/">Mais vistas</a>
             </li>
             <li className={styles.menu__item}>
-                <img scr={novas} alt="" />
+                <img src={novas} alt="" />
                 <a href="/">Novas</a>
             </li>
             <li className={styles.menu__item}>
-                <img scr={surpreenda} alt="" />
+                <img src={surpreenda} alt="" />
                 <a href="/">Surpreenda-me</a>
             </li>
-        </ul>
+        </ul> 
     </nav>
     )
 }
